2bf7874850089b9eb3e928054b85d1d1022600c9
hongyutang <dzkdthy@163.com>
2018-11-20 22:49:18 +0000

    second2 commit

--- app/src/main/java/com/example/root/myapplication/MainActivity.java
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -4,13 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new MapFragment();
                             break;
                         case R.id.bottom_nav_gallary:
-                            selectedFragment = new GalleryFragment();
+                            selectedFragment = new GallaryFragment();
                     }
 
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
